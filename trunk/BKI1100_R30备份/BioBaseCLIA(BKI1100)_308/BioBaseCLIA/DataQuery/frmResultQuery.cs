991e39f8fa93ef0350ffd4b9bc95e94ab17f57e5
1591540375@qq.com <1591540375@qq.com@123d84ec-dc3e-47ae-8767-e8c3c95d7acd>
2021-09-17 02:37:06 +0000

    实验结果修改项目名称显示信息 修改制冷报警返回指令误判的问题 通讯类修改为只返回一条指令的通讯 修改工作列表提示信息错误 仪器维护设置清空温育盘默认选择状态为不选中

--- trunk/BKI1100_R30备份/BioBaseCLIA(BKI1100)_308/BioBaseCLIA/DataQuery/frmResultQuery.cs
+++ trunk/BKI1100_R30备份/BioBaseCLIA(BKI1100)_308/BioBaseCLIA/DataQuery/frmResultQuery.cs
@@ -441,7 +441,11 @@ namespace BioBaseCLIA.DataQuery
                     {
                         dgvSampleData.Rows[i].Selected = true;
                         dr = dtTestResult.NewRow();
-                        dr["ShortName"] = dgvSampleData.Rows[i].Cells["ItemName"].Value.ToString();//dtPro.Select("ShortName = '" + dgvSampleData.Rows[i].Cells["ItemName"].Value.ToString() + "'")[0]["FullName"].ToString();//lyq/*dgvSampleData.Rows[i].Cells["ItemName"].Value.ToString();*/
+                        DataRow[] drPro = dtPro.Select("ShortName = '" + dgvSampleData.Rows[i].Cells["ItemName"].Value.ToString() + "'");
+                        if (drPro.Length > 0)
+                            dr["ShortName"] = drPro[0]["FullName"].ToString();
+                        else
+                            dr["ShortName"] = dgvSampleData.Rows[i].Cells["ItemName"].Value.ToString();//lyq/*dgvSampleData.Rows[i].Cells["ItemName"].Value.ToString();*/
                         dr["Concentration"] = dgvSampleData.Rows[i].Cells["Concentration"].Value.ToString();
                         dr["Result"] = dgvSampleData.Rows[i].Cells["Result"].Value.ToString();
                         dr["Range1"] = dgvSampleData.Rows[i].Cells["Range"].Value.ToString();
